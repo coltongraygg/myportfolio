76c3ce7c9dbd20f1303f85c791cea02cdf89ffcb
coltongraygg <coltongraygg@gmail.com>
2025-03-22 22:09:56 -0500

    update social links

--- src/config.js
+++ src/config.js
@@ -1,26 +1,15 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'colton@graycoding.dev.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/coltongraygg',
     },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
+
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://www.linkedin.com/in/coltong',
     },
   ],
 
@@ -29,10 +18,10 @@ module.exports = {
       name: 'About',
       url: '/#about',
     },
-    {
-      name: 'Experience',
-      url: '/#jobs',
-    },
+    // {
+    //   name: 'Experience',
+    //   url: '/#jobs',
+    // },
     {
       name: 'Work',
       url: '/#projects',
