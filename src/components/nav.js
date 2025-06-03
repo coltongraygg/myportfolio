460074cefacc7261d3a0429e445e7fee9006695d
coltongraygg <coltongraygg@gmail.com>
2025-06-02 23:38:42 -0500

    updated resume fr this time

--- src/components/nav.js
+++ src/components/nav.js
@@ -208,7 +208,7 @@ const Nav = ({ isHome }) => {
   const ResumeLink = (
     <a
       className="resume-button"
-      href="https://drive.google.com/file/d/1Wu8wGiFezjFqdIwudz1psmz2iBZSKX6V/view?usp=sharing"
+      href="https://drive.google.com/file/d/1IyNO06rvXpWvD1sij7V6W00vqxcBtqa1/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer">
       Resume
